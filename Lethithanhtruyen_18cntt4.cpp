740b45b4740d1924c98e57c91a3a795b3edebf03
Tien <nlthtien@gmail.com>
2021-01-06 12:50:03 +0700

    Update Lethithanhtruyen_18cntt4.cpp

--- Lethithanhtruyen_18cntt4.cpp
+++ Lethithanhtruyen_18cntt4.cpp
@@ -2,144 +2,83 @@
 #include <iostream>
 #include <conio.h>
 #include <math.h>
-#include<vector>
 
-struct so{
-	int chuc,donvi;
-};
 using namespace std;
 
 #ifdef WIN32
 void gotoxy(int x, int y)
 {
-  COORD cur = {x, y};
-  SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), cur);
+  	COORD cur = {x, y};
+  	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), cur);
 } 
 #else
 void gotoxy(int x, int y)
 {
-  printf("\033[%dG\033[%dd", x+1, y+1);
+  	printf("\033[%dG\033[%dd", x+1, y+1);
 }
 #endif
 
 int textcolor(int Color)
 {
-  HANDLE h;
-  h = GetStdHandle(STD_OUTPUT_HANDLE); 
-  return SetConsoleTextAttribute(h, Color);
+  	HANDLE h;
+  	h = GetStdHandle(STD_OUTPUT_HANDLE); 
+  	return SetConsoleTextAttribute(h, Color);
 }
 
 #define CYAN 10
 #define YELLOW 14
+
 // baitoanbacthang
-int main(){
- ios_base::sync_with_stdio(0);
- int n,k;
- cin>>n>>k;
- for(int i=1;i<=n;i++) a[i]=1;
- for(int i=1;i<=k;i++) {
- int x;
- cin>>x;
- a[x]=0;
- }
- f[0]=0, f[1]=1;
- for(int i=2;i<=n;i++)
- f[i]=a[i]*(f[i-1]+f[i-2])%Mod;
- cout<<f[n];
- return 0;
-}
-//bai toan ban dia
-int main()
+void Baitap0()
 {
- scanf("%lld",&n);
- for (int i=1;i<=n;i++) scanf("%lld",&a[i]);
- d[1]=a[1];
- f[1]=a[1];
- for (int i=2;i<=n;i++)
- {
- d[i]=max(max(d[i-2],f[i-1])+a[i],d[i-1]);
- f[i]=max(f[i-2],d[i-2])+a[i];
- }
- printf("%lld",d[n]);
- return 0;
+  	system("cls");
+  	int N=100000;
+	int Mod=14062008;
+	bool a[N+1];
+	long long f[N+1];
+	int n,k;
+	 cin>>n>>k;
+	 for(int i=1;i<=n;i++) a[i]=1;
+	 for(int i=1;i<=k;i++) {
+	 int x;
+	 cin>>x;
+	 a[x]=0;
+	 }
+	 f[0]=0, f[1]=1;
+	 for(int i=2;i<=n;i++)
+	 f[i]=a[i]*(f[i-1]+f[i-2])%Mod;
+	 cout<<f[n];
+  	getch();    
 }
-//xau palindrome
-int main(){
- ios_base::sync_with_stdio(0);
- string st;
- cin>>st;
- int n=st.size();
- for(int i=0;i<n;i++) {
- d[i][i]=1;
- if (i<n-1) {
- if (st[i]==st[i+1]) d[i][i+1]=1;
- else d[i][i+1]=2;
- }
- }
- for(int l=2;l<=n;l++){
- for(int i=0;i<n-l;i++){
- int j=i+l;
- d[i][j]=d[i][i]+d[i+1][j];
- if (st[i]==st[j] && d[i+1][j-1]==1)
-d[i][j]=1;
- else
- for(int k=i+1;k<=j;k++)
- d[i][j]=min(d[i][j],d[i][k]+d[k+1][j]);
- }
- }
- cout<<d[0][n-1];
- return 0;
+
+void Baitap1()
+{
+  	system("cls");
+  
+  	getch();    
 }
-//bien doi xau a thanh xau b
-int main(){
-ios_base::sync_with_stdio(0); 
-string a,b;
-cin>>a>>b; d[0][0]=(a[0]!=b[0]);
-for(int i=1;i<a.size();i++) d[i][0]=d[i-1][0]+1;
-for(int i=1;i<b.size();i++) d[0][i]=d[0][i-1]+1;
-for(int i=1;i<a.size();i++)
-for(int j=1;j<b.size();j++)
-if (a[i]==b[j]) d[i][j]=d[i-1][j-1];
-else
-d[i][j]=min(d[i-1][j],min(d[i][j-1],d[i-1][j-1]))+1; cout<<d[a.size()-1][b.size()-1];
-return 0;
+
+void Baitap2()
+{
+  	system("cls");
+   
+  	getch();    
 }
-//select
-long long turnOff(long long s, int j){
-    return s&~(1<<j);
+
+void Baitap3()
+{
+  	system("cls");
+  
+  	getch();  	
 }
-int getBit(long long s, int j)  {
-    return (s >> j)&1;
+
+void Baitap4()
+{
+  	system("cls");
+ 
+  	getch();  	
 }
-long long getMax(long long s){
-    int k=0;
-    for(int j=1;j<=n;j++)
-    if (getBit(s,j-1)==1){
-        k++;
-        b[k]=j;
-    }
-    int Max=0;
-    for(int j=0;j<=k;j++){
-        long long p=turnOff(s,b[j]-1);
-        Max=max(Max,T[p]+a[k][b[j]]);
-    }
-    return Max;
-}
-long long DPBitMask(){
-    long long maxS=pow(2,n)-1;
-    for(int i=1;i<=maxS;i++){
-        T[i]=getMax(i);
-    }
-    return T[maxS];
-}
-int main(){
-    ios_base::sync_with_stdio(0);
-    cin>>n;
-    for(int i=1;i<=n;i++)
-        for(int j=1;j<=n;j++) cin>>a[i][j];
-    cout<<DPBitMask();
-    return 0;
-}
+
 //Viet xau s ra man hinh tai toa do (x,y) voi mau la color
 void Write(char *s,int x,int y, int color)
 {
@@ -150,19 +89,19 @@ void Write(char *s,int x,int y, int color)
 
 void Khung(int x1,int y1,int x2,int y2)
 { int x,y;
-  gotoxy(x1,y1); cout<<"É";
-  gotoxy(x2,y1); cout<<"»";
-  gotoxy(x1,y2); cout<<"È";
-  gotoxy(x2,y2); cout<<"¼";
+  gotoxy(x1,y1); cout<<"Ã‰";
+  gotoxy(x2,y1); cout<<"Â»";
+  gotoxy(x1,y2); cout<<"Ãˆ";
+  gotoxy(x2,y2); cout<<"Â¼";
   for(x=x1+1;x<x2;x++)
 	 {
-		gotoxy(x,y1); cout<<"Í";
-		gotoxy(x,y2); cout<<"Í";
+		gotoxy(x,y1); cout<<"Ã";
+		gotoxy(x,y2); cout<<"Ã";
 	 }
   for(y=y1+1;y<y2;y++)
 	 {
-		gotoxy(x1,y); cout<<"º";
-		gotoxy(x2,y); cout<<"º";
+		gotoxy(x1,y); cout<<"Âº";
+		gotoxy(x2,y); cout<<"Âº";
 	 }
 }
 /*Tao ra menu tai toa do (x0,y0) voi n dong luu trong bien s
@@ -180,6 +119,7 @@ void Ve_menu(int x0,int y0,int chon,int n,char *s[])
   Write("2. DO AN CHUYEN NGANH",x0+40,y0+2,14);
   Write("3.	Lop: 18CNTT4 ",x0+40,y0+3,14);
 
+
 }
 
 main()
@@ -192,7 +132,7 @@ main()
   st[3]="4. Bai toan bien doi xau a thanh xau b";
   st[4]="5. SELECT";
   st[5]="<ESC> Ket thuc chuong trinh!";
-  int x0=5,y0=5,chon=0,luuchon,sodong=4,ok=FALSE;
+  int x0=5,y0=5,chon=0,luuchon,sodong=7,ok=FALSE;
   Ve_menu(x0,y0,chon,sodong,st);
   do
   {
@@ -232,7 +172,15 @@ main()
 			Baitap2();
 			Ve_menu(x0,y0,chon,sodong,st);
 			break;
-		 case 3: exit(0);
+		 case 3:
+			Baitap3();
+			Ve_menu(x0,y0,chon,sodong,st);
+			break;
+		 case 4:
+			Baitap4();
+			Ve_menu(x0,y0,chon,sodong,st);
+			break;
+		 case 5: exit(0);
 		}
 	  ok=FALSE; //tra lai trang thai ENTER chua duoc nhan
 	}
